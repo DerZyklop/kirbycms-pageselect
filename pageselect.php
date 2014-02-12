a56e407c81d07351818a27213952b2b213ec6d1f
DerZyklop <mail@der-zyklop.de>
2014-02-12 11:27:34 +0100

    initial commit

---
+++ pageselect.php
@@ -0,0 +1,36 @@
+<?php
+
+global $site;
+global $options;
+$options = array();
+
+if (!function_exists("makeArray")) {
+
+  function makeArray($items, $layer = 0) {
+    global $options;
+    foreach($items as $item) {
+      $options[count($options)] = $item;
+      if( $item->hasChildren() ) {
+        $layer = $layer++;
+        makeArray($item->children(), $layer);
+      }
+    }
+  }
+
+}
+
+makeArray( $site->pages() );
+?>
+
+<select name="<?php echo $name ?>">
+  <option<?php if(!$value) echo ' selected="selected"' ?> value="">-</option>
+  <?php foreach($options as $key => $item): ?>
+    <?php
+      $seperator = '';
+      for ($i=1; $i < $item->depth(); $i++) {
+        $seperator .= '- ';
+      }
+    ?>
+    <option<?php if($item->uri() == $value) echo ' selected="selected"' ?> value="<?php echo html($item->uri()) ?>"><?php echo($seperator.html($item->title())); ?></option>
+  <?php endforeach ?>
+</select>
